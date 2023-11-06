6a8cef371a700e9c506c136222f3df9d5cef316a
Arthur <arthurleg29@gmail.com>
2023-11-06 22:10:10 +0100

    Modification complète du jeu

--- bataille.py
+++ bataille.py
@@ -1,11 +1,18 @@
+#############################################
+#              Titre: Bataille              #
+#           Auteur : Arthur Le Gall         #
+#            Date: 6 Novembre 2023          #
+#############################################
+
+
 # Importation des classes nécessaires
 from cartes import *
 from joueurs import *
 from jeucartes import *
+import sys
+sys.setrecursionlimit(10000)
 
 # Définition de la classe Bataille
-
-
 class Bataille:
     """
     Classe représentant le jeu de la bataille.
@@ -22,7 +29,8 @@ class Bataille:
     Methods:
     - __init__(self, nbJoueurs=2, nbCartes=52, cartesParJoueur=26): initialise la partie.
     - lireTas(self): retourne une liste de tuples représentant les cartes dans le tas.
-    - jouer(self): joue une partie de la bataille.
+    - egalite(self): gère le cas d'égalité entre les joueurs.
+    - jouer(self, partie=True): joue une partie de la bataille.
     """
 
     def __init__(self, nbJoueurs=2, nbCartes=52, cartesParJoueur=26):
@@ -47,7 +55,7 @@ class Bataille:
         for j in range(nbJoueurs):
             self.joueurs[j].setMain(jeu[j])
         self.tas = []
-        self.partiesMax = 100000
+        self.partiesMax = 10000
         self.parties = 0
 
     def lireTas(self):
@@ -62,63 +70,108 @@ class Bataille:
         """
         return [(carte.getNom(), carte.getCouleur()) for carte in self.tas]
 
-    def jouer(self):
+    def egalite(self):
+        """
+        Si les joueurs ont joué une carte de même valeur alors cette méthode est appelée.        
+        """
+        if self.joueurs[0].getNbCartes() < 2 or self.joueurs[1].getNbCartes() < 2:
+            ### Cas où un des joueurs n'a pas assez de cartes pour continuer la partie
+            if self.joueurs[0].getNbCartes() == 0 or self.joueurs[1].getNbCartes() == 0:
+                print("Fin de jeu, un des joueurs n'a plus de cartes.")
+                tasValeurs = []
+                for i in self.tas:
+                    tasValeurs.append(i.getValeur())
+                gagnant = 0 if tasValeurs.index(
+                    max(tasValeurs)) % 2 == 0 else 1
+                for i in self.tas:
+                    self.joueurs[gagnant].insererUneCarte(i)
+            ### Cas où il a juste assez pour refaire une bataille
+            else:
+                print(
+                    "Egalité mais un des joueurs n'a pas assez de cartes pour continuer la partie.")
+                # Bataille UNE CARTE
+                carteJoueur1 = self.joueurs[0].jouerCarte()
+                carteJoueur2 = self.joueurs[1].jouerCarte()
+                self.tas.append(carteJoueur1)
+                self.tas.append(carteJoueur2)
+                if carteJoueur1.estSuperieureA(carteJoueur2):
+                    for i in self.tas:
+                        self.joueurs[0].insererUneCarte(i)
+                    self.tas = []
+                    print(
+                        f"Vainqueur de la manche: {self.joueurs[0].getNom()}, ({self.joueurs[0].getNbCartes()}, {self.joueurs[1].getNbCartes()}, {self.joueurs[0].getNbCartes() + self.joueurs[1].getNbCartes()}))")
+                elif carteJoueur1.estInferieureA(carteJoueur2):
+                    for i in self.tas:
+                        self.joueurs[1].insererUneCarte(i)
+                    print(
+                        f"Vainqueur de la manche: {self.joueurs[1].getNom()}, ({self.joueurs[0].getNbCartes()}, {self.joueurs[1].getNbCartes()}, {self.joueurs[0].getNbCartes() + self.joueurs[1].getNbCartes()}))")
+                else:
+                    self.jouer(False)
+        ### Cas où le joueur a assez de cartes pour continuer la partie
+        else:
+            print("Egalité, bataille !")
+            carteJoueur1 = self.joueurs[0].jouerCarte()
+            carteJoueur2 = self.joueurs[1].jouerCarte()
+            self.tas.append(carteJoueur1)
+            self.tas.append(carteJoueur2)
+            carteJoueur1 = self.joueurs[0].jouerCarte()
+            carteJoueur2 = self.joueurs[1].jouerCarte()
+            self.tas.append(carteJoueur1)
+            self.tas.append(carteJoueur2)
+            if carteJoueur1.estSuperieureA(carteJoueur2):
+                for i in self.tas:
+                    self.joueurs[0].insererUneCarte(i)
+                self.tas = []
+                print(
+                    f"Vainqueur de la manche: {self.joueurs[0].getNom()}, ({self.joueurs[0].getNbCartes()}, {self.joueurs[1].getNbCartes()}, {self.joueurs[0].getNbCartes() + self.joueurs[1].getNbCartes()}))")
+            elif carteJoueur1.estInferieureA(carteJoueur2):
+                for i in self.tas:
+                    self.joueurs[1].insererUneCarte(i)
+                self.tas = []
+                print(
+                    f"Vainqueur de la manche: {self.joueurs[1].getNom()}, ({self.joueurs[0].getNbCartes()}, {self.joueurs[1].getNbCartes()}, {self.joueurs[0].getNbCartes() + self.joueurs[1].getNbCartes()}))")
+            else:
+                self.egalite()
+
+    def jouer(self, partie=True):
         """
         Joue une partie de la bataille.
 
         Args:
-        None
+        - partie (bool): True si c'est le début de la partie, False sinon.
 
         Returns:
         None
         """
-        partie = True
-        while partie == True and self.parties < self.partiesMax:
-
-            tasValeurs = []
-            self.tas = []
-            savedTas = []
-
-            for i in range(self.nbJoueurs):
-                carteJouee = self.joueurs[i].jouerCarte()
-                self.tas.append(carteJouee)
-                tasValeurs.append(carteJouee.getValeur())
-
-            if tasValeurs.count(max(tasValeurs)) > 1:
-                print("############## Bataille ! ##############")
-                savedTas = self.tas
-                self.tas = []
-                tasValeurs = []
-                for k in range(self.nbJoueurs):
-                    cartejouee = self.joueurs[k].jouerCarte()
-                    self.tas.append(cartejouee)
-                    tasValeurs.append(cartejouee.getValeur())
-                winner = tasValeurs.index(max(tasValeurs))
-                for p in range(len(self.tas)):
-                    self.joueurs[winner].insererMain(self.tas[p])
-                for q in range(len(savedTas)):
-                    self.joueurs[winner].insererMain(savedTas[q])
-
-            else:
-                winner = tasValeurs.index(max(tasValeurs))
-
+        # Vérification que la partie n'est pas terminée
+        if partie:
+            print(
+                f"Début de la partie. ({self.joueurs[0].getNbCartes()}, {self.joueurs[1].getNbCartes()}, {self.joueurs[0].getNbCartes() + self.joueurs[1].getNbCartes()})")
+            # Création des cartes
+            carteJoueur1 = self.joueurs[0].jouerCarte()
+            carteJoueur2 = self.joueurs[1].jouerCarte()
+            # Vérification des cartes et assignation en conséquence
+            if carteJoueur1.estSuperieureA(carteJoueur2):
+                self.joueurs[0].insererMain(carteJoueur1, carteJoueur2)
                 print(
-                    f"Le joueur {self.joueurs[winner].getNom()} a gagné le tour. Le tas était {self.lireTas()}")
-
-                for j in range(len(self.tas)):
-                    self.joueurs[winner].insererMain(self.tas[j])
-
-            for k in range(self.nbJoueurs):
-                if self.joueurs[k].getNbCartes() == 0:
-                    print("Le joueur", self.joueurs[k].getNom(), "a perdu !")
-                    partie = False
-                else:
-                    print("Le joueur", self.joueurs[k].getNom(
-                    ), "a", self.joueurs[k].getNbCartes(), "cartes.")
+                    f"Vainqueur de la manche: {self.joueurs[0].getNom()}, ({self.joueurs[0].getNbCartes()}, {self.joueurs[1].getNbCartes()}, {self.joueurs[0].getNbCartes() + self.joueurs[1].getNbCartes()}))")
+            elif carteJoueur1.estInferieureA(carteJoueur2):
+                self.joueurs[1].insererMain(carteJoueur2, carteJoueur1)
+                print(
+                    f"Vainqueur de la manche: {self.joueurs[1].getNom()}, ({self.joueurs[0].getNbCartes()}, {self.joueurs[1].getNbCartes()}, {self.joueurs[0].getNbCartes() + self.joueurs[1].getNbCartes()}))")
+            else:
+                self.tas.append(carteJoueur1)
+                self.tas.append(carteJoueur2)
+                self.egalite()
+        # Vérification que la partie n'est pas terminée
+        if self.parties < self.partiesMax and self.joueurs[0].getNbCartes() > 0 and self.joueurs[1].getNbCartes() > 0:
             self.parties += 1
-
-            if self.parties == self.partiesMax:
-                print("Nombre de parties maximum atteint !")
+            self.jouer()
+        # Si la partie est terminée
+        else:
+            print(
+                f"Fin de la partie. Le gagnant est le {self.joueurs[0].getNom() if self.joueurs[0].getNbCartes() > 0 else self.joueurs[1].getNom()} avec ses {self.joueurs[0].getNbCartes() if self.joueurs[0].getNbCartes() > 0 else self.joueurs[1].getNbCartes()} cartes.")
+            partie = False
 
 
 # Création d'une partie de bataille avec 2 joueurs par défaut
--- joueurs.py
+++ joueurs.py
@@ -3,54 +3,87 @@ from cartes import *
 from jeucartes import *
 
 # Définit la classe Joueur
+
+
 class Joueur:
     # Constructeur de la classe Joueur
     def __init__(self, nom, nbCartes=0):
+        """
+        Initialise un objet Joueur avec un nom et un nombre de cartes
+        """
         self.nom = nom
         self.nbCartes = nbCartes
         self.main = []
-    
+
     # Setter pour la main du joueur
     def setMain(self, main):
+        """
+        Modifie la main du joueur
+        """
         self.main = main
         self.nbCartes = len(main)
-    
+
     # Getter pour le nom du joueur
     def getNom(self):
+        """
+        Renvoie le nom du joueur
+        """
         return self.nom
-    
+
     # Getter pour le nombre de cartes du joueur
     def getNbCartes(self):
+        """
+        Renvoie le nombre de cartes du joueur
+        """
         return self.nbCartes
-    
+
     # Méthode pour jouer une carte, retourne la dernière carte du jeu et la supprime de la main du joueur
     def jouerCarte(self):
+        """
+        Joue la dernière carte de la main du joueur et la supprime de sa main
+        """
         if len(self.main) == 0:
             return None
         else:
             value = self.main.pop(-1)
             self.nbCartes = len(self.main)
             return value
-    
+
     # Méthode pour insérer une carte dans la main du joueur, au début de sa main
-    def insererMain(self, carte):
+    def insererMain(self, carte1, carte2=None):
+        """
+        Insère une ou deux cartes dans la main du joueur, au début de sa main
+        """
+        self.main.insert(0, carte1)
+        self.main.insert(0, carte2)
+        self.nbCartes = len(self.main)
+
+    def insererUneCarte(self, carte):
+        """
+        Insère une carte dans la main du joueur, au début de sa main
+        """
         self.main.insert(0, carte)
         self.nbCartes = len(self.main)
-    
+
     # Méthode pour afficher la main du joueur
     def montrerMain(self):
+        """
+        Renvoie une liste de tuples contenant le nom et la couleur de chaque carte de la main du joueur
+        """
         return [(carte.getNom(), carte.getCouleur()) for carte in self.main]
-            
-        
+
+
 # Fonction de test pour la classe Joueur
 def testJoueur():
-    """ Test de la classe Joueur, on vérifie :
-            - la création d'un joueur avec un nom correct
-            - la distribution de cartes à un joueur
-            - l'affichage du nom et du nombre de cartes d'un joueur
-            - l'affichage de la main d'un joueur
-            - le fait de jouer une carte
-            - le fait d'insérer une carte dans la main d'un joueur"""
+    """ 
+    Test de la classe Joueur, on vérifie :
+        - la création d'un joueur avec un nom correct
+        - la distribution de cartes à un joueur
+        - l'affichage du nom et du nombre de cartes d'un joueur
+        - l'affichage de la main d'un joueur
+        - le fait de jouer une carte
+        - le fait d'insérer une carte dans la main d'un joueur
+    """
     # Crée un joueur avec le nom "Joueur 1"
     joueur1 = Joueur("Joueur 1")
     # Affiche le nom du joueur
@@ -72,4 +105,3 @@ def testJoueur():
     # Insère une carte dans la main du joueur 1 et l'affiche
     joueur1.insererMain(Carte('As', 'PIQUE'))
     print("Main du joueur 1 : ", joueur1.montrerMain())
-    
